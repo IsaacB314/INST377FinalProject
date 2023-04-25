f017c21beab8d49588d6846bdb558594cb262b58
Vim314 <94699324+IsaacB314@users.noreply.github.com>
2023-04-25 09:52:09 -0400

    Editing Website, added new API

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-$.ajax({
-    method: 'GET',
-    url: 'https://api.api-ninjas.com/v1/country?max_gdp_growth',
-    headers: { 'X-Api-Key': '1nbT8IdaS6tbrn0BCOobWg==TnBCgvjU92HHLXP9'},
-    contentType: 'application/json',
-    success: function(result) {
-        console.log(result);
-    },
-    error: function ajaxError(jqXHR) {
-        console.error('Error: ', jqXHR.responseText);
-    }
-});
+const api_url = 'https://api.v2.emissions-api.org'
+        + '/api/v2/carbonmonoxide/average.json'
+        + '?country=DE&begin=2019-02-01&end=2019-03-01'
+
+
+fetch(api_url)
+.then(response => response.json())
+.then(data => {
+    let values = data.map(x => x.average);
+    console.log(values);
+    // logs:
+    // (28) [0.0330089744766232, 0.032249495510353, 0.0360124611289537, …
+})
