c5576914dcd823ddd3e9b18ceb52bad807e38b54
Vim314 <94699324+IsaacB314@users.noreply.github.com>
2023-04-26 22:16:18 -0400

    Adding 2 Basic Graphs to Test API

--- script.js
+++ script.js
@@ -1,13 +1,94 @@
 const api_url = 'https://api.v2.emissions-api.org'
-        + '/api/v2/carbonmonoxide/average.json'
-        + '?country=DE&begin=2019-02-01&end=2019-03-01'
+        + '/api/v2/methane/average.json'
+        + '?country=US&begin=2019-02-01&end=2019-03-01'
 
+const api_url2 = 'https://api.v2.emissions-api.org'
+        + '/api/v2/nitrogendioxide/average.json'
+        + '?country=FR&begin=2019-02-01&end=2019-03-01'
 
+//Testing basic plot making with US data from 2019
 fetch(api_url)
 .then(response => response.json())
 .then(data => {
     let values = data.map(x => x.average);
     console.log(values);
-    // logs:
-    // (28) [0.0330089744766232, 0.032249495510353, 0.0360124611289537, …
-})
+    let ctx = document.getElementById('average-example').getContext('2d');
+        new Chart(ctx, {
+            type: 'bar',
+            data: {
+                // use start times contained in the requested data as labels
+                labels: data.map(x => x.start.substring(8, 10)),
+                datasets: [{
+                    label: 'United States of America',
+                    backgroundColor: '#93bd22',
+                    // use the average values as data
+                    data: data.map(x => x.average),
+                }]
+            },
+
+            // add a few sensible configuration options
+            options: {
+                scales: {
+                    yAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Methane'
+                        },
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }],
+                    xAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Day'
+                        }
+                    }]
+                }
+            }
+        });
+    })
+
+
+    fetch(api_url2)
+    .then(response => response.json())
+    .then(data => {
+        let values = data.map(x => x.average);
+        console.log(values);
+        let ctx2 = document.getElementById('average-example2').getContext('2d');
+            new Chart(ctx2, {
+                type: 'bar',
+                data: {
+                    // use start times contained in the requested data as labels
+                    labels: data.map(x => x.start.substring(8, 10)),
+                    datasets: [{
+                        label: 'France',
+                        backgroundColor: '#93bd22',
+                        // use the average values as data
+                        data: data.map(x => x.average),
+                    }]
+                },
+    
+                // add a few sensible configuration options
+                options: {
+                    scales: {
+                        yAxes: [{
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Nitrogen Dioxide'
+                            },
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }],
+                        xAxes: [{
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Day'
+                            }
+                        }]
+                    }
+                }
+            });
+        })
+    
